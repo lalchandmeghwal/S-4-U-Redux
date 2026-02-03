c98749ba2605f6dc38da2d326cda48e2a572d1f2
lalchandmeghwal <lalalsrp3336@gmail.com>
2026-02-03 12:41:42 +0530

    something change

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const app = () => {
+  return (
+    <div>
+      <h1 className='text-center text-7xl text-red-600'>Lal Chand Mewghwal</h1>
+    </div>
+  )
+}
+
+export default app
